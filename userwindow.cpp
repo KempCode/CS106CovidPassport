f1b70a0c85521c02663c459137e7e912f3e10979
NeSimix <simeontanesha@gmail.com>
2021-11-02 19:15:23 +1300

    user small

--- userwindow.cpp
+++ userwindow.cpp
@@ -1,11 +1,18 @@
 #include "userwindow.h"
 #include "ui_userwindow.h"
+#include "citezin.h"
+#include "citezinreportissuedialog.h"
+#include "covidpassportdialog.h"
+
 
 UserWindow::UserWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::UserWindow)
 {
     ui->setupUi(this);
+    //    connect(ui->actionNewCitezin, &QAction::triggered,
+    //this, &MainWindow::handleNewCitezin);
+
 }
 
 UserWindow::~UserWindow()
